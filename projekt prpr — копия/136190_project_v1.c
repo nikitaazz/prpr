d9f9606d49a70bbd064428985d2a91ff4fd846e0
Nikita <75564714+nikitaazz@users.noreply.github.com>
2024-10-15 20:05:26 +0200

    apdate

---
+++ projekt prpr — копия/136190_project_v1.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+
+void v1(FILE **DataPointer1,FILE **ParsePointer1,FILE **StringPointer1) {
+    char *StringValues = (char*) calloc(256 , sizeof(char)),*CombainedValues = (char*) calloc(256 , sizeof(char));
+    char *token,buffer[256];
+    int WhileCounter=0;
+    if (*DataPointer1==NULL)
+    {
+        *DataPointer1=fopen("data.txt","r");
+        if (*DataPointer1==NULL)
+        {
+            printf("V1: Unopened txt files.");
+            return;
+        }
+    }
+    if (*ParsePointer1==NULL)
+    {
+        *ParsePointer1=fopen("parse.txt","r");
+        if (*ParsePointer1==NULL)
+        {
+            printf("V1: Unopened txt files.");
+            return;
+        }
+    }
+    if (*StringPointer1==NULL)
+    {
+        *StringPointer1=fopen("string.txt","r");
+        if (*StringPointer1==NULL)
+        {
+            printf("V1: Unopened txt files.");
+            return;
+        }
+    }    
+    while (fgets(buffer, sizeof(buffer),*DataPointer1)!=NULL)
+    {
+        WhileCounter=0;
+        token=strtok(buffer," ");
+        printf("ID. mer. modulu: %s",fgets(StringValues, 256, *StringPointer1));
+        while (token!=NULL)
+        {
+            WhileCounter++;
+            if (WhileCounter==3)
+            {
+                printf("Hodnota 1: %d\n",atoi(token));
+            }
+            if (WhileCounter==4)
+            {
+                printf("Hodnota 2: %lf\n",atof(token));
+            }   
+            token = strtok(NULL, " ");
+        }
+        printf("Poznámka: %s\n",fgets(CombainedValues, 256, *ParsePointer1));
+    }
+    free(StringValues);
+    free(CombainedValues);
+}
+
+void v(FILE **DataPointer1,FILE **ParsePointer1,FILE **StringPointer1) {
+    int NumberOfFunction;
+    scanf("%d",&NumberOfFunction);
+    if (NumberOfFunction==1)
+    {
+        v1(DataPointer1,ParsePointer1,StringPointer1);
+    }
+}
+
+
+int main(void)
+{
+    FILE *DataPointer = NULL;
+    FILE *ParsePointer = NULL;
+    FILE *StringPointer = NULL;
+    FILE **DataPointer1 = &DataPointer;
+    FILE **ParsePointer1 = &ParsePointer;
+    FILE **StringPointer1 = &StringPointer;
+    char CalledFunction;
+    while (1)
+    {
+        scanf("%c",&CalledFunction);
+        if (CalledFunction=='v')
+        {
+            v(DataPointer1,ParsePointer1,StringPointer1);
+            break;
+        }
+    }
+    return 0;
+}
